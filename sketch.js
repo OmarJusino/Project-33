0ec41279a20c7898fd033508b483bb82e5c85673
OmarJusino <80798115+OmarJusino@users.noreply.github.com>
2021-06-25 20:53:07 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 10, 10);
+  createSprite(300, 150, 10,10);
+  
+}
+
+function draw() {
+  background("black");  
+  drawSprites();
+}
+
+function snowAnimation(){
+if(keyDown(SPACE)){
+  }
+}
